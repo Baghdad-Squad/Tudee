0f85f6257c006d0330fb9cc63b4fc5fae4129642
Aboud <gh.abd.com.207@gmail.com>
2025-06-14 00:07:03 +0300

    add a data class for tudee text style

--- app/src/main/java/com/baghdad/tudee/ui/designSystem/font/TudeeTextStyle.kt
+++ app/src/main/java/com/baghdad/tudee/ui/designSystem/font/TudeeTextStyle.kt
@@ -1,3 +1,18 @@
 package com.baghdad.tudee.ui.designSystem.font
 
-data class TudeeTextStyle()
+import androidx.compose.ui.text.TextStyle
+
+data class TudeeTextStyle(
+    val headline: SizedTextStyle,
+    val title: SizedTextStyle,
+    val body: SizedTextStyle,
+    val label: SizedTextStyle,
+){
+
+    data class SizedTextStyle(
+        val large: TextStyle,
+        val medium: TextStyle,
+        val small: TextStyle
+    )
+
+}
