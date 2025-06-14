493f440a8875a0d99a55ac20231464750637eccd
Malak Raef <malak.raef20@gmail.com>
2025-06-14 22:36:54 +0300

    finish slider composable design

---
+++ app/src/main/java/com/baghdad/tudee/ui/composable/Slider.kt
@@ -0,0 +1,89 @@
+package com.baghdad.tudee.ui.composable
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.BasicText
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.baghdad.tudee.designSystem.theme.Theme
+import com.baghdad.tudee.designSystem.theme.TudeeTheme
+
+@Composable
+fun Slider(
+    title: String,
+    description: String,
+    icon: Int,
+    image: Int,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 24.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                BasicText(
+                    text = title,
+                    style = Theme.typography.title.small.copy(
+                        color = Theme.color.textColor.title.copy(alpha = 0.87f)
+                    )
+                )
+                Image(
+                    painter = painterResource(id = icon),
+                    contentDescription = null,
+                    modifier = Modifier.size(20.dp)
+                )
+            }
+            BasicText(
+                text = description,
+                style = Theme.typography.body.small.copy(
+                    color = Theme.color.textColor.body.copy(alpha = 0.6f)
+                )
+            )
+
+        }
+        Image(
+            painter = painterResource(id = image),
+            contentDescription = null,
+            modifier = Modifier
+                .height(92.dp)
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun SliderPreview() {
+    TudeeTheme {
+        Slider(
+            title = "Start working!",
+            description = "You've completed 3 out of 10 tasks \n Keep going!",
+            icon = com.baghdad.tudee.R.drawable.ic_okay_feedback,
+            image = com.baghdad.tudee.R.drawable.happy_robot,
+            modifier = Modifier.background(Color.White)
+        )
+    }
+}
