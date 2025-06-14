b8e982e2d8263877015f88b9cfdd872fc899a0ee
fatima faisal ghazi <fatima.faisal.ghazi@gmail.com>
2025-06-14 19:59:18 +0300

    implement the secondary Buttons

---
+++ app/src/main/java/com/baghdad/tudee/ui/composable/button/DisabledSecondaryButton.kt
@@ -0,0 +1,43 @@
+package com.baghdad.tudee.ui.composable.button
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.text.BasicText
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.baghdad.tudee.designSystem.theme.Theme
+
+
+@Composable
+fun DisabledSecondaryButton(
+    text: String, modifier: Modifier = Modifier,
+    color: Brush = Brush.verticalGradient(
+        listOf(
+            Color.White,
+            Color.White
+        )
+    )
+) {
+    BasicButton(
+        borderRadius = 100.dp,
+        borderStroke =  BorderStroke(1.dp, Theme.color.textColor.stroke)
+        ,
+        onClick = {}, backgroundColor = color,
+        modifier = modifier
+
+    ) {
+        Row() {
+
+            BasicText(
+                text = text,
+                style = Theme.typography.label.large.copy(color = Theme.color.textColor.stroke)
+
+            )
+
+        }
+
+    }
+}
---
+++ app/src/main/java/com/baghdad/tudee/ui/composable/button/NormalSecondaryButton.kt
@@ -0,0 +1,48 @@
+package com.baghdad.tudee.ui.composable.button
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.BasicText
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.baghdad.tudee.R
+import com.baghdad.tudee.designSystem.theme.Theme
+
+
+@Composable
+fun NormalSecondaryButton(
+    text: String, modifier: Modifier = Modifier,
+    color: Brush = Brush.verticalGradient(
+        listOf(
+            Color.White,
+            Color.White
+        )
+    )
+) {
+    BasicButton(
+        borderRadius = 100.dp,
+        borderStroke =  BorderStroke(1.dp, Theme.color.textColor.stroke)
+        ,
+        onClick = {}, backgroundColor = color,
+        modifier = modifier
+
+    ) {
+        Row() {
+
+            BasicText(
+                text = text,
+                style = Theme.typography.label.large.copy(color = Theme.color.primaryColor.normal)
+
+            )
+
+        }
+
+    }
+}
