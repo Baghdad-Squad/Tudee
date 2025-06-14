a27075f440ab1f34763de63d6894449e4d2b96f2
fatima faisal ghazi <fatima.faisal.ghazi@gmail.com>
2025-06-15 00:33:22 +0300

    refactor BasicButton

--- app/src/main/java/com/baghdad/tudee/ui/composable/button/BasicButton.kt
+++ app/src/main/java/com/baghdad/tudee/ui/composable/button/BasicButton.kt
@@ -8,12 +8,11 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
@@ -26,43 +25,48 @@ import com.baghdad.tudee.designSystem.theme.Theme
 fun BasicButton(
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
-    backgroundColor: Brush = Brush.linearGradient(listOf( Theme.color.primaryColor.normal,Theme.color.primaryColor.normal)),
+    backgroundColor: Brush = Brush.linearGradient(
+        listOf(
+            Theme.color.primaryColor.normal,
+            Theme.color.primaryColor.normal
+        )
+    ),
     borderRadius: Dp = 8.dp,
     shape: Shape = RoundedCornerShape(borderRadius),
     borderStroke: BorderStroke? = null,
     padding: PaddingValues = PaddingValues(12.dp),
     content: @Composable () -> Unit
 ) {
-        Box(
-            modifier = modifier
-                .clickable(onClick = onClick)
-                .background(
-                    brush = backgroundColor,
-                    shape = shape
-                )
-                .let { currentModifier ->
-                    if (borderStroke != null) {
-                        currentModifier.border(
-                            border = borderStroke,
-                            shape = shape
-                        )
-                    } else {
-                        currentModifier
-                    }
-                }
-                .padding(padding),
-            contentAlignment = Alignment.Center
+    Box(
+        modifier = modifier
+            .background(
+                brush = backgroundColor,
+                shape = shape
+            )
+            .let { currentModifier ->
+                if (borderStroke != null) {
+                    currentModifier.border(
+                        border = borderStroke,
+                        shape = shape
+                    )
+                } else {
+                    currentModifier
+                }.clip(shape)
+            }
+            .clickable(onClick = onClick)
+            .padding(padding),
+        contentAlignment = Alignment.Center
 
-        ) {
-            content()
-        }
+    ) {
+        content()
     }
+}
 
 @Preview(showBackground = true)
 @Composable
 fun ExampleUsage() {
     BasicButton(
-        onClick = {  },
+        onClick = { },
         borderStroke = BorderStroke(2.dp, Color.Black),
         borderRadius = 12.dp
     ) {
