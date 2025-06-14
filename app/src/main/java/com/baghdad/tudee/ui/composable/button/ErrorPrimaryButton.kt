71618789f348985a0d9b6d7a8f5865d3d3d1b854
fatima faisal ghazi <fatima.faisal.ghazi@gmail.com>
2025-06-15 00:32:10 +0300

    implement the ErrorPrimaryButton

---
+++ app/src/main/java/com/baghdad/tudee/ui/composable/button/ErrorPrimaryButton.kt
@@ -0,0 +1,28 @@
+package com.baghdad.tudee.ui.composable.button
+
+import androidx.compose.foundation.text.BasicText
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.unit.dp
+import com.baghdad.tudee.designSystem.theme.Theme
+
+@Composable
+fun ErrorPrimaryButton(
+    text: String, modifier: Modifier = Modifier,
+    color: Brush = Brush.verticalGradient(listOf(Theme.color.status.errorVariant,
+        Theme.color.status.errorVariant))
+) {
+    BasicButton(
+        borderRadius = 100.dp,
+        onClick = {}, backgroundColor = color,
+        modifier = modifier
+
+    ) {
+        BasicText(
+            text = text,
+            style = Theme.typography.label.large.copy(color=Theme.color.status.error)
+
+        )
+    }
+}
