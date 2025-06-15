094fa7cf6e1de6af8767c0d3f78b43a994eee668
edrees <muhammed.edrees101@gmail.com>
2025-06-15 20:25:27 +0300

    refactor PrimaryButton

--- app/src/main/java/com/baghdad/tudee/ui/composable/button/PrimaryButton.kt
+++ app/src/main/java/com/baghdad/tudee/ui/composable/button/PrimaryButton.kt
@@ -1,63 +1,90 @@
 package com.baghdad.tudee.ui.composable.button
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.width
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.baghdad.tudee.R
 import com.baghdad.tudee.designSystem.theme.Theme
+import com.baghdad.tudee.designSystem.theme.TudeeTheme
+import com.baghdad.tudee.ui.composable.StripedCircularProgressIndicator
 
 @Composable
 fun PrimaryButton(
-    text: String,
+    label: String,
     modifier: Modifier = Modifier,
-    icon: Int = R.drawable.ic_white_loading,
-    color: Brush = Theme.color.primaryColor.gradient,
     onClick: () -> Unit = {},
-    isLoading: Boolean = true,
-    isDisabled: Boolean = false,
-    size: Dp = 64.dp,
-    borderRadius: Dp = 100.dp,
-    disabledColorBackgroundColor: Brush = Brush.verticalGradient(
-        listOf(
-            Theme.color.textColor.disable,
-            Theme.color.textColor.disable
-        )
-    ),
-    disabledColorTextColor: TextStyle =Theme.typography.label.large.copy(Theme.color.textColor.stroke)
+    isLoading: Boolean = false,
+    isEnabled: Boolean = true,
+    isError: Boolean = false,
 ) {
-    val backgroundColor = if (isDisabled) disabledColorBackgroundColor else color
-    val textColor = if (isDisabled) disabledColorTextColor else  Theme.typography.label.large.copy(color = Theme.color.textColor.onPrimary)
-
+    val buttonColors =  when {
+        isEnabled.not() -> ButtonColors(
+            backgroundColor = Theme.color.textColor.disable,
+            contentColor = Theme.color.textColor.stroke
+        )
+        isError -> ButtonDefaults.colors(
+            backgroundColor = Theme.color.status.errorVariant,
+            contentColor = Theme.color.status.error
+        )
+        else -> ButtonColors(
+            backgroundGradient = Theme.color.primaryColor.gradient,
+            contentColor = Theme.color.textColor.onPrimary
+        )
+    }
     BasicButton(
-        borderRadius = borderRadius,
-        onClick = onClick, backgroundColor = backgroundColor,
-        modifier = modifier, isLoading = isLoading,
-        isDiabled = isDisabled
-    ) {
-        Row() {
-            BasicText(
-                text = text,
-                style = textColor
+        onClick = onClick,
+        modifier = modifier,
+        enabled = isEnabled,
+        contentPadding = PaddingValues(vertical = 18.dp, horizontal = 24.dp),
+        colors = buttonColors,
+        shape = RoundedCornerShape(100.dp),
+    ){
+        BasicText(
+            text = label,
+            style = Theme.typography.label.large.copy(
+                color = buttonColors.contentColor
             )
-            if (isLoading) {
-                Spacer(Modifier.width(10.dp))
-                Image(
-                    painter = painterResource(icon), contentDescription = null,
-                    colorFilter = ColorFilter.tint(
-                        Theme.color.textColor.onPrimary
-                    )
-                )
-            }
+        )
+        AnimatedVisibility(isLoading) {
+            StripedCircularProgressIndicator(
+                modifier = Modifier.padding(start = 8.dp),
+                size = 18.dp,
+                lineLength = 5.dp,
+                lineWidth = 2.dp,
+                color = buttonColors.contentColor
+            )
         }
     }
+}
+
+@Preview(showBackground = true,)
+@Composable
+private fun PrimaryButtonPreview1() {
+    TudeeTheme {
+        PrimaryButton(label = "Submit")
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+private fun PrimaryButtonPreview2() {
+    PrimaryButton("Submit", isLoading = true)
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun PrimaryButtonPreview3() {
+    PrimaryButton("Submit", isError = true)
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun PrimaryButtonPreview4() {
+    PrimaryButton("Submit", isEnabled = false)
 }
