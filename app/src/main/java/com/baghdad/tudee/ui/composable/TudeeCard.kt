25e8fa519519ddb04f91ac115eb484ce1c4ba851
Arzo-zi <zinahashraf88@gmail.com>
2025-06-16 22:37:18 +0300

    refactor: use Text instead of basicText

--- app/src/main/java/com/baghdad/tudee/ui/composable/TudeeCard.kt
+++ app/src/main/java/com/baghdad/tudee/ui/composable/TudeeCard.kt
@@ -8,11 +8,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicText
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -33,16 +32,18 @@ fun TudeeCard(
             .border(
                 width = 1.dp,
                 color = Theme.color.textColor.onPrimaryStroke,
+                shape = RoundedCornerShape(32.dp)
             )
-            .clip(RoundedCornerShape(32.dp))
-            .background(color = Theme.color.textColor.onPrimaryCard)
+            .background(
+                color = Theme.color.textColor.onPrimaryCard, shape = RoundedCornerShape(32.dp)
+            )
     ) {
         Box(
             modifier = Modifier
                 .padding(top = 24.dp, start = 16.dp, end = 16.dp, bottom = 48.dp)
                 .height(120.dp)
         ) {
-            BasicText(
+            Text(
                 text = title,
                 style = Theme.typography.title.medium.copy(
                     color = Theme.color.textColor.title,
@@ -52,14 +53,13 @@ fun TudeeCard(
 
             Spacer(modifier = Modifier.height(16.dp))
 
-            BasicText(
+            Text(
                 text = description,
                 style = Theme.typography.body.medium.copy(
                     color = Theme.color.textColor.body,
                     textAlign = TextAlign.Center,
                 ),
                 modifier = Modifier.align(Alignment.BottomCenter)
-
             )
         }
     }
