ffb575842a9e60cbaa09544a53284aa972a91450
Baraa Abdulrahman <baraaabdulrahman68@gmail.com>
2025-06-16 22:01:20 +0300

    refactor the category

--- app/src/main/java/com/baghdad/tudee/ui/composable/Category.kt
+++ app/src/main/java/com/baghdad/tudee/ui/composable/Category.kt
@@ -1,150 +1,71 @@
 package com.baghdad.tudee.ui.composable
-
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.baghdad.tudee.R
-import com.baghdad.tudee.designSystem.color.lightThemeColor
-import com.baghdad.tudee.designSystem.textStyle.tudeeTextStyle
 import com.baghdad.tudee.designSystem.theme.Theme
-import com.baghdad.tudee.ui.composable.Priority
-data class TaskItemData(
-    val iconRes: Painter,
-    val iconBackgroundColor: Color,
-    val title: String,
-    val description: String,
-    val priority: String
-)
-
+import com.baghdad.tudee.ui.utils.noRippleClickable
 
 @Composable
-fun TaskInfo(title: String, description: String, modifier: Modifier = Modifier) {
-    Column(
-        modifier = modifier,
-        verticalArrangement = Arrangement.spacedBy(2.dp)
-    ) {
-        Text(
-            text = title,
-            modifier = Modifier
-                .width(296.dp)
-                .height(19.dp),
-            color = Theme.color.textColor.body,
-            style = tudeeTextStyle.body.medium,
-            maxLines = 1,
-        )
-        Text(
-            text = description,
-            modifier = Modifier
-                .fillMaxWidth(),
-            color = Theme.color.textColor.title,
-            style = tudeeTextStyle.label.small,
-            maxLines = 1,
-            overflow = TextOverflow.Ellipsis
-        )
-    }
-}
-
-@Composable
-fun TaskItem(item: TaskItemData, modifier: Modifier = Modifier) {
-    Column(
-        modifier = Modifier
-            .width(320.dp)
-            .height(111.dp)
-            .background(
-                color = Theme.color.surfaceColor.surfaceHigh,
-                shape = RoundedCornerShape(16.dp)
-            )
-            .border(
-                width = 1.dp,
-                color = Theme.color.surfaceColor.surfaceHigh,
-                shape = RoundedCornerShape(16.dp)
-            )
-            .padding(
-                top = 4.dp,
-                end = 12.dp,
-                bottom = 12.dp,
-                start = 4.dp
-            ),
-        verticalArrangement = Arrangement.spacedBy(2.dp)
-    ) {
-
-        TaskImage(
-            item = item,
-            imagePainter = item.iconRes
-        )
-        TaskInfo(
-            title = item.title,
-            description = item.description
-        )
-        Priority(
-            priorityTask = when (item.priority) {
-                "High" -> PriorityTask.HIGH
-                "Medium" -> PriorityTask.MEDIUM
-                else -> PriorityTask.LOW
-            }
-        )
-    }
-}
-
-@Composable
-fun TaskImage(
-item: TaskItemData,
-imagePainter: Painter,
-modifier: Modifier = Modifier
+fun CategoryTaskCard(
+    title: String,
+    description: String,
+    priorityTask: PriorityTask,
+    icon: Painter,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
 ) {
-    Box(
-        modifier = Modifier
-            .size(304.dp, 56.dp),
-        contentAlignment = Alignment.TopStart
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(Theme.color.surfaceColor.surfaceHigh, shape = RoundedCornerShape(16.dp))
+            .padding(horizontal = 12.dp, vertical = 12.dp)
+            .noRippleClickable {
+                onClick()
+            }
     ) {
         Row(
-            modifier = Modifier
-                .offset(285.dp, 25.dp)
-                .size(248.dp, 28.dp)
+            Modifier
+                .fillMaxWidth()
+            ,
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            Priority(
-                priorityTask = when (item.priority) {
-                    "High" -> PriorityTask.HIGH
-                    "Medium" -> PriorityTask.MEDIUM
-                    else -> PriorityTask.LOW
-                }
+            Box(Modifier.size(56.dp), contentAlignment = Alignment.Center) {
+                Image(
+                    painter = icon,
+                    contentDescription = "Category Icon",
+                )
+            }
+            TaskPriority(
+                priorityTask = priorityTask,
             )
         }
-        Image(
-            painter = painterResource(
-                id = R.drawable.ic_pur_book
-            ),
-            contentDescription = item.title,
-            modifier = Modifier.size(56.dp)
-        )
 
+        Column(modifier = Modifier.padding(top = 2.dp)) {
+            Text(
+                text = title,
+                style = Theme.typography.label.large,
+                color = Theme.color.textColor.body,
+            )
+            Text(
+                text = description,
+                style = Theme.typography.label.small,
+                color = Theme.color.textColor.hint,
+                modifier = Modifier.padding(top = 2.dp)
+            )
+        }
     }
 }
