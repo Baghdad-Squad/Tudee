0a962742de03bfda0beeafd90b17b9549f05754a
Abdulaziz Anwer <the.azizanwer@gmail.com>
2025-06-16 22:07:54 +0300

    make the padding perfect for the textfield

--- app/src/main/java/com/baghdad/tudee/ui/composable/TudeeTextField.kt
+++ app/src/main/java/com/baghdad/tudee/ui/composable/TudeeTextField.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Icon
@@ -26,7 +27,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.baghdad.tudee.R
 import com.baghdad.tudee.designSystem.theme.Theme
 
 @Composable
@@ -83,6 +87,10 @@ fun TudeeTextField(
                 Modifier
                     .fillMaxWidth()
                     .align(Alignment.CenterVertically)
+                    .then(
+                       if (leadingIcon == null) Modifier.padding(top = 5.dp)
+                       else Modifier.align(Alignment.CenterVertically)
+                    )
             ) {
                 if (value.isEmpty() && !isFocused) {
                     Text(
@@ -114,5 +122,18 @@ fun VerticalSeparator(){
             .background(Theme.color.textColor.stroke)
             .fillMaxHeight()
             .width(1.dp)
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TudeeTextFieldPreview(modifier: Modifier = Modifier){
+    var name by remember { mutableStateOf("") }
+    TudeeTextField(
+        value = name,
+        onValueChange = { name = it },
+        hint = "aziz",
+        modifier = modifier,
+        leadingIcon = painterResource(id = R.drawable.ic_use),
     )
 }
