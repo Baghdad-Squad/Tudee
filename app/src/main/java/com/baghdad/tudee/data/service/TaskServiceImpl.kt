8c118ef09671eff8914f60cb663439edf2dab2ca
Baraa Abdulrahman <baraaabdulrahman68@gmail.com>
2025-06-17 19:51:19 +0300

    finish dao and service of task implementation

--- app/src/main/java/com/baghdad/tudee/data/service/TaskServiceImpl.kt
+++ app/src/main/java/com/baghdad/tudee/data/service/TaskServiceImpl.kt
@@ -4,6 +4,7 @@ import android.database.sqlite.SQLiteDatabaseCorruptException
 import android.database.sqlite.SQLiteException
 import android.database.sqlite.SQLiteFullException
 import com.baghdad.tudee.data.database.dao.TaskDao
+import com.baghdad.tudee.data.model.TaskDto
 import com.baghdad.tudee.domain.entity.Task
 import com.baghdad.tudee.domain.exception.DatabaseCorruptException
 import com.baghdad.tudee.domain.exception.DatabaseException
@@ -20,47 +21,29 @@ import kotlin.uuid.Uuid
 class TaskServiceImpl(
     private val taskDao: TaskDao
 ) : TaskService , BaseService() {
-    override suspend fun getTasksByCategory(categoryId: Uuid): Flow<List<Task>> {
-        val flow = executeWithErrorHandling {
+    override suspend fun getTasksByCategory(categoryId: String): Flow<List<Task>> {
+        return executeWithErrorHandling {
             taskDao.getTasksByCategory(categoryId)
         }
-        return flow.catch { e ->
-            throw when (e) {
-                is SQLiteFullException -> StorageFullException(e.message)
-                is SQLiteDatabaseCorruptException -> DatabaseCorruptException(e.message)
-                is SQLiteException -> DatabaseException(e.message)
-                else -> TudeeException(e.message)
-            }
-        }
     }
 
-    override suspend fun getTasksByDate(date: LocalDate): Flow<List<Task>> {
-        val flow = executeWithErrorHandling {
-            taskDao.getTasksByDate(date)
-        }
-        return flow.catch { e ->
-            throw when (e) {
-                is SQLiteFullException -> StorageFullException(e.message)
-                is SQLiteDatabaseCorruptException -> DatabaseCorruptException(e.message)
-                is SQLiteException -> DatabaseException(e.message)
-                else -> TudeeException(e.message)
-            }
-        }
+    override suspend fun getTasksByDate(date: String): Flow<List<Task>> {
+        return taskDao.getTasksByDate(date)
     }
 
-    override suspend fun createTask(task: Task) {
+    override suspend fun createTask(task: TaskDto) {
         executeWithErrorHandling {
             taskDao.createTask(task)
         }
     }
 
-    override suspend fun editTask(task: Task) {
+    override suspend fun editTask(task: TaskDto) {
         executeWithErrorHandling {
             taskDao.editTask(task)
         }
     }
 
-    override suspend fun deleteTask(taskId: Uuid) {
+    override suspend fun deleteTask(taskId: String) {
         executeWithErrorHandling {
             taskDao.deleteTask(taskId)
         }
