15eba1cf4e3e322b82521c6adf042c89ffa9ddda
Baraa Abdulrahman <baraaabdulrahman68@gmail.com>
2025-06-17 13:20:48 +0300

    initialized room db classes

---
+++ app/src/main/java/com/baghdad/tudee/data/database/TudeeDatabase.kt
@@ -0,0 +1,6 @@
+package com.baghdad.tudee.data.database
+
+class TudeeDatabase {
+
+
+}
---
+++ app/src/main/java/com/baghdad/tudee/data/database/dao/TaskDao.kt
@@ -0,0 +1,8 @@
+package com.baghdad.tudee.data.database.dao
+
+import androidx.room.Dao
+
+@Dao
+interface TaskDao {
+
+}
