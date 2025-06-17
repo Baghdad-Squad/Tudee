4143926e5f553ead1d5901792c92fa9361aeffdf
Baraa Abdulrahman <baraaabdulrahman68@gmail.com>
2025-06-17 18:09:18 +0300

    finish dao and service of task implementation

--- app/src/main/java/com/baghdad/tudee/data/database/dao/TaskDao.kt
+++ app/src/main/java/com/baghdad/tudee/data/database/dao/TaskDao.kt
@@ -1,8 +1,39 @@
 package com.baghdad.tudee.data.database.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.baghdad.tudee.data.model.TaskDto
+import com.baghdad.tudee.domain.entity.Task
+import kotlinx.coroutines.flow.Flow
+import kotlinx.datetime.LocalDate
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
 
 @Dao
+@OptIn(ExperimentalUuidApi::class)
 interface TaskDao {
+    @Insert
+    suspend fun insertTask(task: TaskDto)
 
+
+    @Query("SELECT * FROM tasks WHERE categoryId = :categoryId")
+    suspend fun getTasksByCategory(categoryId: Uuid): Flow<List<Task>>
+
+
+    @Query("SELECT * FROM tasks WHERE date = :date")
+    suspend fun getTasksByDate(date: LocalDate): Flow<List<Task>>
+
+
+    @Insert
+    suspend fun createTask(task: Task)
+
+    @Update
+    suspend fun editTask(task: Task)
+
+
+    @Delete
+    suspend fun deleteTask(taskId: Uuid)
 }
--- app/src/main/java/com/baghdad/tudee/data/service/TaskServiceImpl.kt
+++ app/src/main/java/com/baghdad/tudee/data/service/TaskServiceImpl.kt
@@ -1,32 +1,71 @@
 package com.baghdad.tudee.data.service
 
+import android.database.sqlite.SQLiteDatabaseCorruptException
+import android.database.sqlite.SQLiteException
+import android.database.sqlite.SQLiteFullException
 import com.baghdad.tudee.data.database.dao.TaskDao
 import com.baghdad.tudee.domain.entity.Task
+import com.baghdad.tudee.domain.exception.DatabaseCorruptException
+import com.baghdad.tudee.domain.exception.DatabaseException
+import com.baghdad.tudee.domain.exception.StorageFullException
+import com.baghdad.tudee.domain.exception.TudeeException
 import com.baghdad.tudee.domain.service.TaskService
 import kotlinx.coroutines.flow.Flow
-import java.time.LocalDate
-import java.util.UUID
+import kotlinx.coroutines.flow.catch
+import kotlinx.datetime.LocalDate
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
 
+@OptIn(ExperimentalUuidApi::class)
 class TaskServiceImpl(
     private val taskDao: TaskDao
-): TaskService {
-    override suspend fun getTasksByCategory(categoryId: UUID): Flow<List<Task>> {
-        TODO("Not yet implemented")
+) : TaskService , BaseService() {
+    override suspend fun getTasksByCategory(categoryId: Uuid): Flow<List<Task>> {
+        val flow = executeWithErrorHandling {
+            taskDao.getTasksByCategory(categoryId)
+        }
+        return flow.catch { e ->
+            throw when (e) {
+                is SQLiteFullException -> StorageFullException(e.message)
+                is SQLiteDatabaseCorruptException -> DatabaseCorruptException(e.message)
+                is SQLiteException -> DatabaseException(e.message)
+                else -> TudeeException(e.message)
+            }
+        }
     }
 
     override suspend fun getTasksByDate(date: LocalDate): Flow<List<Task>> {
-        TODO("Not yet implemented")
+        val flow = executeWithErrorHandling {
+            taskDao.getTasksByDate(date)
+        }
+        return flow.catch { e ->
+            throw when (e) {
+                is SQLiteFullException -> StorageFullException(e.message)
+                is SQLiteDatabaseCorruptException -> DatabaseCorruptException(e.message)
+                is SQLiteException -> DatabaseException(e.message)
+                else -> TudeeException(e.message)
+            }
+        }
     }
 
     override suspend fun createTask(task: Task) {
-        TODO("Not yet implemented")
+        executeWithErrorHandling {
+            taskDao.createTask(task)
+        }
     }
 
     override suspend fun editTask(task: Task) {
-        TODO("Not yet implemented")
+        executeWithErrorHandling {
+            taskDao.editTask(task)
+        }
     }
 
-    override suspend fun deleteTask(taskId: UUID) {
-        TODO("Not yet implemented")
+    override suspend fun deleteTask(taskId: Uuid) {
+        executeWithErrorHandling {
+            taskDao.deleteTask(taskId)
+        }
     }
+
 }
+
+
