96e42b5ef5e623c739a432ee3da281c30f69bb92
Aboud <gh.abd.com.207@gmail.com>
2025-06-17 19:14:54 +0300

    add categoryViewModel to ViewModelModule

--- app/src/main/java/com/baghdad/tudee/di/ViewModelModule.kt
+++ app/src/main/java/com/baghdad/tudee/di/ViewModelModule.kt
@@ -1,7 +1,9 @@
 package com.baghdad.tudee.di
 
+import com.baghdad.tudee.ui.viewModel.CategoryViewModel
+import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.dsl.module
 
 val viewModelModule = module {
-
+    viewModelOf(::CategoryViewModel)
 }
