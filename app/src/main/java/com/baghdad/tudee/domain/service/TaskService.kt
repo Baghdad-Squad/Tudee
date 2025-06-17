e3b269e8a525fe6f9420e1e9a34d79be4e676d85
Baraa Abdulrahman <baraaabdulrahman68@gmail.com>
2025-06-17 20:14:50 +0300

    finish dao and service of task implementation

--- app/src/main/java/com/baghdad/tudee/domain/service/TaskService.kt
+++ app/src/main/java/com/baghdad/tudee/domain/service/TaskService.kt
@@ -1,19 +1,20 @@
 package com.baghdad.tudee.domain.service
 
-import com.baghdad.tudee.data.model.TaskDto
 import com.baghdad.tudee.domain.entity.Task
 import kotlinx.coroutines.flow.Flow
 import kotlinx.datetime.LocalDate
 import kotlin.uuid.ExperimentalUuidApi
-
 import kotlin.uuid.Uuid
+
+
 @OptIn(ExperimentalUuidApi::class)
 interface TaskService {
-
 
    suspend fun getTasksByCategory(categoryId: Uuid): Flow<List<Task>>
    suspend fun getTasksByDate(date: LocalDate): Flow<List<Task>>
    suspend fun createTask(task: Task)
    suspend fun editTask(task: Task)
+
+   @OptIn(ExperimentalUuidApi::class)
    suspend fun deleteTask(taskId: Uuid)
 }
