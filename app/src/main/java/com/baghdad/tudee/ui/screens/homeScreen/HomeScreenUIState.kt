e8d72163f4b03b4561a6cf95ff17e0e3a3dfbbd8
Hasan Ali <ihasanalihasan@gmail.com>
2025-06-17 16:54:21 +0300

    Update HomeScreenUIState to include task lists and UI flags

--- app/src/main/java/com/baghdad/tudee/ui/screens/homeScreen/HomeScreenUIState.kt
+++ app/src/main/java/com/baghdad/tudee/ui/screens/homeScreen/HomeScreenUIState.kt
@@ -1,5 +1,16 @@
 package com.baghdad.tudee.ui.screens.homeScreen
 
+import com.baghdad.tudee.domain.entity.Task
+
 data class HomeScreenUIState(
-    val new : String = ""
+    val inProgressTasks: List<Task> = emptyList(),
+    val todoTasks: List<Task> = emptyList(),
+    val doneTasks: List<Task> = emptyList(),
+    val errorMessage: String? = null,
+    val showAddTaskDialog: Boolean = false,
+    val showAddCategoryDialog: Boolean = false,
+    val isSnakeBarShown: Boolean = false,
+    val isLoading: Boolean = false,
+    val isEmptyTasks: Boolean = false
+
 )
