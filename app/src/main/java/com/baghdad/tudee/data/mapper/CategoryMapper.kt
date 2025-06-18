b8d6987fdcf2b555240941d234ff222a61afc810
edrees <muhammed.edrees101@gmail.com>
2025-06-18 22:19:47 +0300

    add support for multiple image types Category and CategoryDto

--- app/src/main/java/com/baghdad/tudee/data/mapper/CategoryMapper.kt
+++ app/src/main/java/com/baghdad/tudee/data/mapper/CategoryMapper.kt
@@ -3,21 +3,40 @@ package com.baghdad.tudee.data.mapper
 import com.baghdad.tudee.data.model.CategoryDto
 import com.baghdad.tudee.domain.entity.Category
 import kotlin.uuid.ExperimentalUuidApi
-import kotlin.uuid.Uuid
 
 @OptIn(ExperimentalUuidApi::class)
-fun CategoryDto.toEntity(): Category = Category(
-    id = this.id,
-    title = this.title,
-    imageUri = this.imageUri
-)
+fun CategoryDto.toEntity(): Category {
+    val image = when (imageType) {
+        CategoryDto.IMAGE_TYPE_BYTE_ARRAY -> {
+            Category.Image.ByteArray(imageBytes!!)
+        }
+
+        else -> throw IllegalArgumentException("Image type not supported: $imageType")
+    }
+
+    return Category(
+        id = this.id,
+        title = this.title,
+        image = image
+    )
+}
 
 @OptIn(ExperimentalUuidApi::class)
-fun Category.toDto(): CategoryDto = CategoryDto(
-    id = this.id,
-    imageUri = this.imageUri,
-    title = this.title,
-)
+fun Category.toDto(): CategoryDto {
+    return when (image) {
+        is Category.Image.Predefined -> CategoryDto(
+            id = this.id,
+            title = this.title,
+            imageType = CategoryDto.IMAGE_TYPE_PREDEFINED,
+        )
+
+        is Category.Image.ByteArray -> CategoryDto(
+            id = this.id,
+            title = this.title,
+            imageType = CategoryDto.IMAGE_TYPE_BYTE_ARRAY,
+        )
+    }
+}
 
 fun List<CategoryDto>.toEntities(): List<Category> = this.map { it.toEntity() }
 
