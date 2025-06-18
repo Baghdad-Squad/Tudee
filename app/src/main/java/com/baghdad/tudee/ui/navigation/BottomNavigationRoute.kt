a3fb7a12a5d200fb24c7c16730724689b38222d7
Hasan Ali <ihasanalihasan@gmail.com>
2025-06-18 15:07:48 +0300

    rename AppScreen to BottomNavigationRoute and convert to enum class for improved navigation structure

--- app/src/main/java/com/baghdad/tudee/ui/navigation/BottomNavigationRoute.kt
+++ app/src/main/java/com/baghdad/tudee/ui/navigation/BottomNavigationRoute.kt
@@ -1,32 +1,32 @@
 package com.baghdad.tudee.ui.navigation
 
 import com.baghdad.tudee.R
+import com.baghdad.tudee.domain.entity.Task
 
-
-sealed class AppScreen (
-    val route : String,
+enum class BottomNavigationRoute(
+    val route: Route,
     val selectedIcon: Int? = null,
     val unSelectedIcon: Int? = null,
-){
-    data object OnBoardingScreen : AppScreen(route = "on_boarding_screen")
-
-    data object HomeScreen : AppScreen(
-        route = "home_screen",
+) {
+    HOME_SCREEN(
+        route = Route.HomeScreen,
         selectedIcon = R.drawable.ic_blue_home,
         unSelectedIcon = R.drawable.ic_black_home,
-    )
+    ),
 
-    data object TasksScreen : AppScreen(
-        route = "tasks_screen",
+   TASK_SCREEN(
+        route = Route.TasksScreen(taskState = Task.State.IN_PROGRESS,),
         selectedIcon = R.drawable.ic_blue_note,
         unSelectedIcon = R.drawable.ic_black_note,
-        )
+    ),
 
-    data object CategoriesScreen : AppScreen(
-        route = "categories_screen",
+    CATEGORIES_SCREEN(
+        route = Route.CategoriesScreen,
         selectedIcon = R.drawable.ic_blue_menu,
         unSelectedIcon = R.drawable.ic_menu_circle,
     )
 }
+
+
 
 
