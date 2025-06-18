075f54b7b3b2397294e79fbf251db645eb91f465
Abdulaziz Anwer <the.azizanwer@gmail.com>
2025-06-18 14:01:59 +0300

    add category part using list of fake data of category to implement it

---
+++ app/src/main/java/com/baghdad/tudee/ui/addTask/TaskBottomSheetContent.kt
@@ -0,0 +1,142 @@
+package com.baghdad.tudee.ui.addTask
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.baghdad.tudee.data.fakeData.fakeCategoriesData
+import com.baghdad.tudee.designSystem.theme.Theme
+import com.baghdad.tudee.domain.entity.Category
+import com.baghdad.tudee.domain.entity.Task
+import com.baghdad.tudee.ui.addTask.composable.MainButtonPart
+import com.baghdad.tudee.ui.addTask.composable.PriorityButtonPart
+import com.baghdad.tudee.ui.addTask.composable.TextFieldScreenPart
+import com.baghdad.tudee.ui.composable.CategoryItem
+import com.baghdad.tudee.ui.composable.TudeeBottomSheet
+import com.baghdad.tudee.ui.composable.button.PrimaryButton
+import com.baghdad.tudee.ui.composable.button.SecondaryButton
+import kotlin.uuid.ExperimentalUuidApi
+
+
+@OptIn(ExperimentalUuidApi::class)
+@Composable
+fun TaskBottomSheet(
+    initial: Task? = null,
+    state: List<Category>
+) {
+    var titleText by remember { mutableStateOf("") }
+    var paragraphText by remember { mutableStateOf("") }
+    var dateTime by remember { mutableStateOf("") }
+    var selectedCategoryId by remember { mutableStateOf(initial?.categoryId) }
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        LazyColumn(
+            modifier = Modifier.weight(1f),
+            contentPadding = PaddingValues(vertical = 16.dp)
+        ) {
+            item {
+                TextFieldScreenPart(
+                    title = initial?.title ?: titleText,
+                    onTitleChange = { titleText = it },
+                    paragraph = paragraphText,
+                    onParagraphChange = { paragraphText = it },
+                    dateTime =  dateTime,
+                    onDateTimeChange = { dateTime = it }
+                )
+
+                PriorityButtonPart()
+            }
+
+            item {
+                Column(
+                    modifier = Modifier.padding(horizontal = 16.dp)
+                ) {
+                    Text(
+                        text = "Category",
+                        style = Theme.typography.title.medium
+                    )
+                }
+
+                LazyVerticalGrid(
+                    columns = GridCells.Adaptive(104.dp),
+                    contentPadding = PaddingValues(
+                        start = 16.dp,
+                        end = 16.dp,
+                        top = 12.dp,
+                        bottom = 12.dp
+                    ),
+                    userScrollEnabled = false,
+                    verticalArrangement = Arrangement.spacedBy(25.dp),
+                    horizontalArrangement = Arrangement.spacedBy(8.dp),
+                    modifier = Modifier.height(300.dp)
+                ) {
+                    items(state) { category ->
+                        CategoryItem(
+                            label = category.title,
+                            icon = painterResource(id = category.imageUri.toInt()),
+                            onClick = {
+                                selectedCategoryId = if (selectedCategoryId == category.id) {
+                                    null
+                                } else {
+                                    category.id
+                                }
+                            },
+                            isSelected = selectedCategoryId == category.id
+                        )
+                    }
+                }
+            }
+        }
+
+        MainButtonPart(titleText, paragraphText, dateTime)
+    }
+}
+
+@Composable
+fun ShowTaskSheetButton() {
+    var showSheet by remember { mutableStateOf(false) }
+    Box(modifier = Modifier.fillMaxSize()) {
+        PrimaryButton(
+            onClick = { showSheet = true },
+            label = "open",
+            modifier = Modifier.align(Alignment.Center)
+        )
+        TudeeBottomSheet(
+            isVisible = showSheet,
+            onDismiss = { showSheet = false }
+        ) {
+            TaskBottomSheet(
+                state = fakeCategoriesData()
+            )
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun myprivew(){
+    ShowTaskSheetButton()
+}
