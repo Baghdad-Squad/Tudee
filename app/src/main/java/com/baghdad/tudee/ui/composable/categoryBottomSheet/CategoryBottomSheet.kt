bb2c78ca26aa52fb179cf35b977487723dd4619b
Hager Abobakr <hagarabobakr1@gmail.com>
2025-06-18 03:39:39 +0300

    add CategoryBottomSheet file

---
+++ app/src/main/java/com/baghdad/tudee/ui/composable/categoryBottomSheet/CategoryBottomSheet.kt
@@ -0,0 +1,21 @@
+package com.baghdad.tudee.ui.composable.categoryBottomSheet
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun CategoryBottomSheet(){
+    Box {
+
+    }
+
+}
+
+
+
+@Preview
+@Composable
+fun CategoryBottomSheetPreview(){
+
+}
