ed440e6df6bfe28b945e16aaff5a280a0b7d6ca5
Aboud <gh.abd.com.207@gmail.com>
2025-06-19 02:25:33 +0300

    refactor category ui state, convert image type to byte array

--- app/src/main/java/com/baghdad/tudee/ui/viewModel/state/CategoryUiState.kt
+++ app/src/main/java/com/baghdad/tudee/ui/viewModel/state/CategoryUiState.kt
@@ -1,11 +1,10 @@
 package com.baghdad.tudee.ui.viewModel.state
 
 import kotlin.uuid.ExperimentalUuidApi
-import kotlin.uuid.Uuid
 
 data class CategoryUiState @OptIn(ExperimentalUuidApi::class) constructor(
-    val id: Uuid,
+    val id: Long? = null,
     val title: String,
-    val imageUri: String,
+    val image: ByteArray,
     val taskCount: Int? = null
 )
