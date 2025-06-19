3314a7354569d125c38b78c6e7cad03d131262c0
Aboud <gh.abd.com.207@gmail.com>
2025-06-20 00:42:09 +0300

    refactor category screen directory

--- app/src/main/java/com/baghdad/tudee/di/ViewModelModule.kt
+++ app/src/main/java/com/baghdad/tudee/di/ViewModelModule.kt
@@ -1,10 +1,9 @@
 package com.baghdad.tudee.di
 
-import com.baghdad.tudee.ui.screens.categoryScreen.CategoryViewModel
+import com.baghdad.tudee.ui.screens.category.CategoryViewModel
 import org.koin.androidx.viewmodel.dsl.viewModelOf
 import com.baghdad.tudee.ui.main.MainViewModel
 import com.baghdad.tudee.ui.screens.tasks.TasksViewModel
-import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.dsl.module
 
 val viewModelModule = module {
