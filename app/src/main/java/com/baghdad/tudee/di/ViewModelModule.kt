426e78d09fe8e767d8687e1397b796c9a0ebd74a
Aboud <gh.abd.com.207@gmail.com>
2025-06-19 14:17:50 +0300

    update category view model path

--- app/src/main/java/com/baghdad/tudee/di/ViewModelModule.kt
+++ app/src/main/java/com/baghdad/tudee/di/ViewModelModule.kt
@@ -1,6 +1,6 @@
 package com.baghdad.tudee.di
 
-import com.baghdad.tudee.ui.viewModel.CategoryViewModel
+import com.baghdad.tudee.ui.screens.categoryScreen.CategoryViewModel
 import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.dsl.module
 
