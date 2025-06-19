fc61bb56802c9132083346abfa8466d8a8215f66
Arzo-zi <zinahashraf88@gmail.com>
2025-06-19 19:59:41 +0300

    clear: unwanted files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,9 +68,9 @@ dependencies {
 
 
     //material 3
-    implementation (libs.material3)
-    implementation (libs.ui)
-    implementation (libs.androidx.runtime)
+    implementation(libs.material3)
+    implementation(libs.ui)
+    implementation(libs.androidx.runtime)
 
     implementation(libs.kotlinx.datetime)
 
