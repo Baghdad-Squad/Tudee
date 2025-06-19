74f319ad071cc9a86c6b0c2b3294ac2e5b00be51
edrees <muhammed.edrees101@gmail.com>
2025-06-19 20:05:17 +0300

    refactor MainActivity and add MainViewModel for dark theme management

--- app/src/main/java/com/baghdad/tudee/di/DatabaseModule.kt
+++ app/src/main/java/com/baghdad/tudee/di/DatabaseModule.kt
@@ -3,6 +3,9 @@ package com.baghdad.tudee.di
 import androidx.room.Room
 import com.baghdad.tudee.data.database.TudeeDatabase
 import com.baghdad.tudee.data.database.callback.DatabaseInitializerCallback
+import com.baghdad.tudee.data.database.dao.AppConfigurationDao
+import com.baghdad.tudee.data.database.dao.CategoryDao
+import com.baghdad.tudee.data.database.dao.TaskDao
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
@@ -14,10 +17,10 @@ val databaseModule = module {
             TudeeDatabase::class.java,
             TudeeDatabase.DATABASE_NAME
         )
-            .addCallback(DatabaseInitializerCallback(get()))
+            .addCallback(DatabaseInitializerCallback { get<CategoryDao>() })
             .build()
     }
-    singleOf(TudeeDatabase::appConfigurationDao)
-    singleOf(TudeeDatabase::categoryDao)
-    singleOf(TudeeDatabase::taskDao)
+    single<CategoryDao> { get<TudeeDatabase>().categoryDao() }
+    single<AppConfigurationDao> { get<TudeeDatabase>().appConfigurationDao() }
+    single<TaskDao> { get<TudeeDatabase>().taskDao() }
 }
--- app/src/main/java/com/baghdad/tudee/di/ViewModelModule.kt
+++ app/src/main/java/com/baghdad/tudee/di/ViewModelModule.kt
@@ -1,10 +1,12 @@
 package com.baghdad.tudee.di
 
+import com.baghdad.tudee.ui.main.MainViewModel
 import com.baghdad.tudee.ui.screens.tasks.TasksViewModel
 import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.dsl.module
 
 val viewModelModule = module {
     viewModelOf(::TasksViewModel)
+    viewModelOf(::MainViewModel)
 
 }
