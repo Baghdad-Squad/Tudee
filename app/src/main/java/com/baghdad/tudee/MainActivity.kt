40a7a97e2056074339710fb0af884aa0501101db
Arzo-zi <zinahashraf88@gmail.com>
2025-06-19 19:47:06 +0300

    feat: add SplashScreen to the project

--- app/src/main/java/com/baghdad/tudee/MainActivity.kt
+++ app/src/main/java/com/baghdad/tudee/MainActivity.kt
@@ -4,41 +4,45 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.baghdad.tudee.designSystem.theme.TudeeTheme
+import com.baghdad.tudee.ui.component.OnboardingScreen
+import com.baghdad.tudee.ui.component.SplashScreen
+import kotlinx.coroutines.delay
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            setContent {
-                TudeeTheme {
+            TudeeTheme {
+
+                val currentTimeOut = remember { mutableStateOf(true) }
+
+                LaunchedEffect(Unit) {
+                    delay(2000)
+                    currentTimeOut.value = false
                 }
-            }
-            }
+                AnimatedVisibility(
+                    currentTimeOut.value
+                ) {
+                    SplashScreen()
+                }
 
+                AnimatedVisibility(
+                    visible = !currentTimeOut.value,
+                    modifier = Modifier.fillMaxSize(),
+                ) {
+                    OnboardingScreen { }
+                }
+
+            }
         }
     }
-
-    @Composable
-    fun Greeting(name: String, modifier: Modifier = Modifier) {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier
-        )
-    }
-
-    @Preview(showBackground = true)
-    @Composable
-    fun GreetingPreview() {
-        TudeeTheme {
-            Greeting("Android")
-        }
-    }
+}
