8f14ff79938a79fd106ac5d11238f45fef7686c6
Hasan Ali <ihasanalihasan@gmail.com>
2025-06-20 05:33:13 +0300

    feat: refactor ViewModelModule to use viewModelOf for HomeScreenViewModel instantiation

--- app/src/main/java/com/baghdad/tudee/di/ViewModelModule.kt
+++ app/src/main/java/com/baghdad/tudee/di/ViewModelModule.kt
@@ -1,14 +1,13 @@
 package com.baghdad.tudee.di
 
 import com.baghdad.tudee.viewModel.homescreenViewModel.HomeScreenViewModel
-import org.koin.androidx.viewmodel.dsl.viewModel
 import com.baghdad.tudee.ui.main.MainViewModel
 import com.baghdad.tudee.ui.screens.tasks.TasksViewModel
 import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.dsl.module
 
 val viewModelModule = module {
-    viewModel{ HomeScreenViewModel(get(), get()) }
+    viewModelOf(::HomeScreenViewModel)
     viewModelOf(::TasksViewModel)
     viewModelOf(::MainViewModel)
 
