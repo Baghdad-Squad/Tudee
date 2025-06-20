180ba909d3c6a59b7f50f17d5524f582112b2d59
Hasan Ali <ihasanalihasan@gmail.com>
2025-06-20 05:26:31 +0300

    feat: add JUnit Jupiter dependency for enhanced testing capabilities

--- app/src/main/java/com/baghdad/tudee/ui/composable/taskDetailsBottomSheet/TaskDetails.kt
+++ app/src/main/java/com/baghdad/tudee/ui/composable/taskDetailsBottomSheet/TaskDetails.kt
@@ -8,11 +8,9 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -35,7 +33,6 @@ fun TaskDetails(
     icon: Painter,
     taskPriority: Task.Priority,
     taskState: Task.State,
-
     ) {
     TudeeBottomSheet(
         isVisible = isVisible,
@@ -115,7 +112,9 @@ fun TaskDetailsContent(
                 else -> ""
             }
 
-            TaskActionsContainer(buttonText = buttonText)
+            TaskActionsContainer(buttonText = buttonText){
+
+            }
         }
     }
 }
