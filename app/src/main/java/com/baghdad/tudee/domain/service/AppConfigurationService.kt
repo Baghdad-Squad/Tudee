d479375587e1970d8f4f8990b60e70f4a8962d9e
edrees <muhammed.edrees101@gmail.com>
2025-06-20 04:31:45 +0300

    add first launch configuration handling

--- app/src/main/java/com/baghdad/tudee/domain/service/AppConfigurationService.kt
+++ app/src/main/java/com/baghdad/tudee/domain/service/AppConfigurationService.kt
@@ -5,4 +5,5 @@ import kotlinx.coroutines.flow.Flow
 interface AppConfigurationService {
     suspend fun isDarkTheme(): Flow<Boolean>
     suspend fun setTheme(isDark: Boolean)
+    suspend fun isFirstLaunch(): Boolean
 }
