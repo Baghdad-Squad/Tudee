d58682d0622ac1aed370fb1830e45d5a9c92cc8f
MahmoodTarek <dev.mahmoudtarek@gmail.com>
2025-06-22 09:07:50 +0200

    fix update category image and task count display issues

--- app/src/main/java/com/baghdad/tudee/ui/composable/TaskSection.kt
+++ app/src/main/java/com/baghdad/tudee/ui/composable/TaskSection.kt
@@ -3,9 +3,7 @@ package com.baghdad.tudee.ui.composable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
