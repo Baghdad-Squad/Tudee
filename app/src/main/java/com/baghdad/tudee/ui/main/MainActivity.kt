bcf8a942a47c679c09908ac9a236d39b29988456
MahmoodTarek <dev.mahmoudtarek@gmail.com>
2025-06-22 06:54:16 +0200

    set status bar icon color based on screen background not device theme

--- app/src/main/java/com/baghdad/tudee/ui/main/MainActivity.kt
+++ app/src/main/java/com/baghdad/tudee/ui/main/MainActivity.kt
@@ -75,19 +75,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TudeeTheme {
-        Greeting("Android")
-    }
-}
