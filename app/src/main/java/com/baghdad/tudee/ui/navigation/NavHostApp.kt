fb875194acc5260154509e3bca8a47ef4eefdda4
Hasan Ali <ihasanalihasan@gmail.com>
2025-06-23 15:29:52 +0300

    Refactor: update HomeScreen navigation to improve task navigation flow

--- app/src/main/java/com/baghdad/tudee/ui/navigation/NavHostApp.kt
+++ app/src/main/java/com/baghdad/tudee/ui/navigation/NavHostApp.kt
@@ -1,10 +1,6 @@
 package com.baghdad.tudee.ui.navigation
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
@@ -65,7 +61,12 @@ fun TudeeNavHost(
         }
 
         composable<Route.HomeScreen> {
-            HomeScreen()
+            HomeScreen(
+                navigateToTasks = {
+                    navController.navigate(Route.TasksScreen())
+                },
+
+            )
         }
 
         composable<Route.CategoryTasksScreen>{
