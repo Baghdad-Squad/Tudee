57e80785a06f5d63ddcaae6e7d1237d6c48c03a3
edrees <muhammed.edrees101@gmail.com>
2025-06-24 20:43:02 +0300

    refactor CategoryMapper

---
+++ app/src/main/java/com/baghdad/tudee/ui/screens/categories/CategoryMapper.kt
@@ -0,0 +1,17 @@
+package com.baghdad.tudee.ui.screens.categories
+
+import com.baghdad.tudee.domain.entity.Category
+import com.baghdad.tudee.ui.model.CategoryUiState
+
+fun Category.toUiState(): CategoryUiState {
+    return CategoryUiState(
+        id = id,
+        title = title,
+        image = when (image) {
+            is Category.Image.ByteArray -> Category.Image.ByteArray(image.data)
+            is Category.Image.Predefined -> Category.Image.Predefined(image.type)
+
+        },
+        isPredefined = isPredefinedCategory
+    )
+}
