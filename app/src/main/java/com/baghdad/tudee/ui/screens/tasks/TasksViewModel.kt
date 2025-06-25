692ffc413ae10ea4301c33f7998c2d0fde95a3fe
Farah Maytham <farah.maytham.ahmed@gmail.com>
2025-06-25 14:29:32 +0300

    modify the date when clicking add task in task screen it will be the selected date tab (#225)
    * modify the date when clicking add task in task screen it will be the selected date tab
    
    * add 0L to task bottom sheet content
    
    ---------
    
    Co-authored-by: fatimafaisalghazi <fatima.faisal.ghazi@gmail.com>

--- app/src/main/java/com/baghdad/tudee/ui/screens/tasks/TasksViewModel.kt
+++ app/src/main/java/com/baghdad/tudee/ui/screens/tasks/TasksViewModel.kt
@@ -125,7 +125,10 @@ class TasksViewModel(
     override fun toggleAddEditTaskDialog(initialTaskId: Long?) {
         val initialTask = initialTaskId?.let { taskId ->
             getTaskById(taskId)
-        }
+        }?: Task(
+            date = uiState.value.selectedDate ?: LocalDate.now(),
+            id = 0, title = "", description = "", priority = Task.Priority.LOW, categoryId = 0, state = Task.State.TODO
+        )
         _uiState.update {
             it.copy(
                 initialTask = initialTask,
