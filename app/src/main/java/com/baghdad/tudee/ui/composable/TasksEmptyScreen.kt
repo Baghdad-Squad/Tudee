4ef5933a09a498bf73c0777738b94bc5dbeb888e
Arzo-zi <zinahashraf88@gmail.com>
2025-06-25 18:25:40 +0300

    refactor: move TasksEmptyScreen to composable file

--- app/src/main/java/com/baghdad/tudee/ui/composable/TasksEmptyScreen.kt
+++ app/src/main/java/com/baghdad/tudee/ui/composable/TasksEmptyScreen.kt
@@ -1,4 +1,4 @@
-package com.baghdad.tudee.ui.screens.tasks.components
+package com.baghdad.tudee.ui.composable
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
