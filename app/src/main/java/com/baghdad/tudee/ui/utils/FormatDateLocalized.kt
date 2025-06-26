f04b63e97cc0f20aa40dc7d290a6a35e2a99a661
Aboud <gh.abd.com.207@gmail.com>
2025-06-27 02:04:45 +0300

    update localization

--- app/src/main/java/com/baghdad/tudee/ui/utils/FormatDateLocalized.kt
+++ app/src/main/java/com/baghdad/tudee/ui/utils/FormatDateLocalized.kt
@@ -10,15 +10,6 @@ fun getLocalizedNumber(number: Number): String {
     return formatter.format(number)
 }
 
-val arabicNumbers = mapOf(
-    '0' to '٠', '1' to '١', '2' to '٢', '3' to '٣', '4' to '٤',
-    '5' to '٥', '6' to '٦', '7' to '٧', '8' to '٨', '9' to '٩'
-)
-
-fun convertToArabicNumbers(input: Int): String {
-    return input.toString().map { arabicNumbers[it] ?: it }.joinToString("")
-}
-
 
 fun formatDateLocalized(date: LocalDate): String {
     val locale = Locale.getDefault()
