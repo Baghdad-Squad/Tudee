55039ced783bd941b2fb1bb2b0da3fbcfeaf8d19
Hasan Ali <ihasanalihasan@gmail.com>
2025-06-26 23:53:17 +0300

    refactor: enhance TextFieldScreenPart to support add/edit mode with dynamic title

--- app/src/main/java/com/baghdad/tudee/ui/composable/PairOfTask.kt
+++ app/src/main/java/com/baghdad/tudee/ui/composable/PairOfTask.kt
@@ -3,6 +3,7 @@ package com.baghdad.tudee.ui.composable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
