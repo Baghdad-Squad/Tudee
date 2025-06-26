351b38649e066277f8241aac42a0dc151b3e420f
edrees <muhammed.edrees101@gmail.com>
2025-06-26 05:58:06 +0300

    simplify category label retrieval and update icon handling

--- app/src/main/java/com/baghdad/tudee/ui/screens/categories/component/CategoriesList.kt
+++ app/src/main/java/com/baghdad/tudee/ui/screens/categories/component/CategoriesList.kt
@@ -6,16 +6,12 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.baghdad.tudee.domain.entity.Category
 import com.baghdad.tudee.ui.composable.CategoryItem
 import com.baghdad.tudee.ui.model.CategoryUiState
 import com.baghdad.tudee.ui.utils.getCategoryIconPainter
-import com.baghdad.tudee.ui.utils.getLabelResId
-import com.baghdad.tudee.ui.utils.image.byteArrayToPainter
+import com.baghdad.tudee.ui.utils.getCategoryTitle
 
 @Composable
 fun CategoriesList(
@@ -37,13 +33,8 @@ fun CategoriesList(
     ) {
         items(categories) {
             CategoryItem(
-                label = when(it.image){
-                    is Category.Image.Predefined -> stringResource((it.image).type.getLabelResId())
-                    is Category.Image.ByteArray -> it.title
-                },
-                icon = getCategoryIconPainter(
-                    it.image ?: Category.Image.Predefined(Category.PredefinedType.ENTERTAINMENT)
-                ),
+                label = getCategoryTitle(it),
+                icon = getCategoryIconPainter(it.image),
                 onClick = {
                     onCategoryClick(it)
                 },
