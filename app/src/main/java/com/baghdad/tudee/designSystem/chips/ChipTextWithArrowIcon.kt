479a604813d796aa322a80d73b9389170ec88a4a
Hasan Ali <ihasanalihasan@gmail.com>
2025-06-26 14:18:08 +0300

    add composables for mood display and interaction in home screen

---
+++ app/src/main/java/com/baghdad/tudee/designSystem/chips/ChipTextWithArrowIcon.kt
@@ -0,0 +1,52 @@
+package com.baghdad.tudee.designSystem.chips
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.baghdad.tudee.R
+import com.baghdad.tudee.designSystem.theme.Theme
+import com.baghdad.tudee.ui.utils.noRippleClickable
+
+@Composable
+fun ChipTextWithArrowIcon(
+    numberOfItem: Int,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+) {
+
+    Row(
+        modifier
+            .background(
+                Theme.color.surfaceColor.surfaceHigh,
+                shape = RoundedCornerShape(100.dp)
+            )
+            .padding(horizontal = 8.dp, vertical = 6.dp)
+            .noRippleClickable {
+                onClick()
+            },
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(4.dp)
+    ) {
+        Text(
+            text = "$numberOfItem",
+            style = Theme.typography.label.medium,
+            color = Theme.color.textColor.body
+        )
+        Icon(
+            painter = painterResource(R.drawable.ic_arrow),
+            contentDescription = "Arrow Icon",
+            tint = Theme.color.textColor.body
+        )
+
+    }
+
+}
