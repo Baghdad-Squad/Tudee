d117082495cb4930a34a6dba475fa69fbf7b1fc9
Hasan Ali <ihasanalihasan@gmail.com>
2025-06-26 23:54:37 +0300

    refactor: replace hardcoded content description with localized string in ChipTextWithArrowIcon and update HomeScreen layout to use TudeeScaffold

--- app/src/main/java/com/baghdad/tudee/designSystem/chips/ChipTextWithArrowIcon.kt
+++ app/src/main/java/com/baghdad/tudee/designSystem/chips/ChipTextWithArrowIcon.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.baghdad.tudee.R
 import com.baghdad.tudee.designSystem.theme.Theme
@@ -43,7 +44,7 @@ fun ChipTextWithArrowIcon(
         )
         Icon(
             painter = painterResource(R.drawable.ic_arrow),
-            contentDescription = "Arrow Icon",
+            contentDescription = stringResource(R.string.arrow_icon),
             tint = Theme.color.textColor.body
         )
 
--- app/src/main/java/com/baghdad/tudee/ui/screens/homeScreen/HomeScreen.kt
+++ app/src/main/java/com/baghdad/tudee/ui/screens/homeScreen/HomeScreen.kt
@@ -6,21 +6,16 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -45,6 +40,7 @@ import com.baghdad.tudee.ui.composable.SnakeBar
 import com.baghdad.tudee.ui.composable.TasksEmptyScreen
 import com.baghdad.tudee.ui.composable.TopTudeeBar
 import com.baghdad.tudee.ui.composable.TudeeBottomSheet
+import com.baghdad.tudee.ui.composable.TudeeScaffold
 import com.baghdad.tudee.ui.composable.button.FloatingActionButton
 import com.baghdad.tudee.ui.composable.taskDetailsBottomSheet.TaskDetailsBottomSheet
 import com.baghdad.tudee.ui.screens.homeScreen.addEditTask.AddEditTaskBottomSheet
@@ -64,42 +60,49 @@ fun HomeScreen(navigateToTaskScreen: (Task.State) -> Unit, modifier: Modifier = 
 fun HomeScreenContent(navigateToTaskScreen: (Task.State) -> Unit, modifier: Modifier = Modifier) {
     val viewModel: HomeScreenViewModel = koinViewModel()
     val state by viewModel.state.collectAsState()
-    Scaffold(
+
+
+    TudeeScaffold(
+        topBar = {
+            TopTudeeBar(
+                title = "Tudee",
+                description = stringResource(R.string.Your_personal_task_manager),
+                isDay = state.isDark.not(),
+                onChangeTheme = {
+                    viewModel.onClickSwitchTheme()
+                }
+            )
+        },
         floatingActionButton = {
             FloatingActionButton(
                 painter = painterResource(R.drawable.ic_add),
                 onClick = {
                     viewModel.toggleAddNewTaskDialog()
                 },
-                modifier = Modifier.padding(16.dp)
+            )
+        },
+        snackbar = {
+            SnakeBar(
+                message = state.showSnackBar.message,
+                isSuccess = !state.showSnackBar.isError,
+                isVisible = state.showSnackBar.isVisible
             )
         }
-
-
     ) {
+
+
         BottomSheetHandler(state, viewModel)
-        it
-        Box(modifier = modifier.fillMaxSize()) {
-            Column(
-                modifier
-                    .fillMaxSize()
-                    .background(Theme.color.primaryColor.normal)
-                    .padding(WindowInsets.statusBars.asPaddingValues())
-            ) {
-                TopTudeeBar(
-                    title = "Tudee",
-                    description = stringResource(R.string.Your_personal_task_manager),
-                    isDay = state.isDark.not(),
-                    onChangeTheme = {
-                        viewModel.onClickSwitchTheme()
-                    }
-                )
+        Box(modifier = modifier.fillMaxSize()
+            .background(Theme.color.primaryColor.normal)
+        ) {
+
                 LazyColumn(
                     Modifier
                         .fillMaxSize()
                         .background(Theme.color.surfaceColor.surface)
                 ) {
                     item { StatusTasksSection(state, modifier = Modifier.fillParentMaxWidth()) }
+
                     if (state.inProgressTasks.isNotEmpty())
                         item {
                             HorizontalTaskSection(
@@ -108,7 +111,10 @@ fun HomeScreenContent(navigateToTaskScreen: (Task.State) -> Unit, modifier: Modi
                                 tasks = state.inProgressTasks,
                                 state = state,
                                 viewModel = viewModel,
-                                modifier = Modifier.fillParentMaxWidth().padding(bottom = 8.dp),
+                                modifier = Modifier
+                                    .offset(y = -22.dp)
+                                    .fillParentMaxWidth()
+                                    .padding(bottom = 8.dp),
                                 onClick = { navigateToTaskScreen(Task.State.IN_PROGRESS) }
                             )
                         }
@@ -120,45 +126,40 @@ fun HomeScreenContent(navigateToTaskScreen: (Task.State) -> Unit, modifier: Modi
                                 tasks = state.todoTasks,
                                 state = state,
                                 viewModel = viewModel,
-                                modifier = Modifier.fillParentMaxWidth().padding(bottom = 8.dp),
+                                modifier = Modifier
+                                    .offset(y = -22.dp)
+                                    .fillParentMaxWidth()
+                                    .padding(bottom = 8.dp),
                                 onClick = { navigateToTaskScreen(Task.State.TODO) }
                             )
                         }
                     if (state.doneTasks.isNotEmpty())
                         item {
-                            Spacer(modifier = Modifier.height(16.dp))
                             HorizontalTaskSection(
                                 name = stringResource(R.string.done),
                                 numberOfItem = state.doneTasks.size,
                                 tasks = state.doneTasks,
                                 state = state,
                                 viewModel = viewModel,
-                                modifier = Modifier.fillParentMaxWidth().padding(bottom = 8.dp),
+                                modifier = Modifier
+                                    .offset(y = -22.dp)
+
+                                    .fillParentMaxWidth()
+                                    .padding(bottom = 8.dp),
                                 onClick = { navigateToTaskScreen(Task.State.DONE) }
                             )
                         }
                     if (state.todoTasks.isEmpty() && state.inProgressTasks.isEmpty() && state.doneTasks.isEmpty()) {
                         item { TasksEmptyScreen() }
                     }
-
                 }
             }
-            SnakeBar(
-                Modifier
-                    .padding(horizontal = 16.dp)
-                    .align(Alignment.TopCenter)
-                    .padding(top = 120.dp),
-                message = state.showSnackBar.message,
-                isSuccess = !state.showSnackBar.isError,
-                isVisible = state.showSnackBar.isVisible
-            )
-
         }
-    }
+
 }
 
 @Composable
- fun TextDateIcon(
+fun TextDateIcon(
     text: String,
     modifier: Modifier = Modifier,
     icon: Painter,
@@ -184,8 +185,6 @@ fun HomeScreenContent(navigateToTaskScreen: (Task.State) -> Unit, modifier: Modi
 
     }
 }
-
-
 
 
 @Composable
@@ -267,6 +266,7 @@ fun TextHeadTaskSection(
 
     }
 }
+
 @Composable
 private fun StatusTasksSection(state: HomeScreenUIState, modifier: Modifier = Modifier) {
     Box(
@@ -281,7 +281,7 @@ private fun StatusTasksSection(state: HomeScreenUIState, modifier: Modifier = Mo
             .padding(horizontal = 16.dp)
             .offset(y = -45.dp)
             .background(
-                Theme.color.surfaceColor.surface,
+                Theme.color.surfaceColor.surfaceHigh,
                 shape = RoundedCornerShape(16.dp)
             )
     ) {
@@ -295,8 +295,9 @@ private fun StatusTasksSection(state: HomeScreenUIState, modifier: Modifier = Mo
         OverViewSection(state)
     }
 }
+
 @Composable
-private fun OverViewSection(state: HomeScreenUIState){
+private fun OverViewSection(state: HomeScreenUIState) {
     Text(
         text = stringResource(R.string.overview),
         style = Theme.typography.title.large,
@@ -341,7 +342,7 @@ fun HorizontalTaskSection(
             if (tasks.isNotEmpty()) {
                 items(tasks.chunked(2)) { pair ->
                     PairOfTask(
-                        modifier = Modifier.fillParentMaxWidth(0.95f),
+                        modifier = Modifier.fillParentMaxWidth(0.97f),
                         pair = pair,
                         state = state,
                         viewModel = viewModel,
