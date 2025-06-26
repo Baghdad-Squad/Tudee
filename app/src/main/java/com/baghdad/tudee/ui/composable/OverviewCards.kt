d6dc2c0fbd58eec1dedc7be3e8354345ac702fd6
Hasan Ali <ihasanalihasan@gmail.com>
2025-06-26 14:17:26 +0300

    add OverviewCards and PairOfTask composables for enhanced task display

---
+++ app/src/main/java/com/baghdad/tudee/ui/composable/OverviewCards.kt
@@ -0,0 +1,132 @@
+package com.baghdad.tudee.ui.composable
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
+import com.baghdad.tudee.R
+import com.baghdad.tudee.designSystem.theme.Theme
+import com.baghdad.tudee.ui.screens.homeScreen.HomeScreenUIState
+import com.baghdad.tudee.ui.screens.homeScreen.TaskState
+import com.baghdad.tudee.ui.utils.insideBorder
+
+@Composable
+fun OverviewCards(
+    state: HomeScreenUIState,
+    modifier: Modifier = Modifier,
+) {
+
+    val overViews = listOf(
+        OverViewCard(TaskState.TODO, Theme.color.status.purpleAccent, state.todoTasks.size),
+        OverViewCard(
+            TaskState.IN_PROGRESS,
+            Theme.color.status.yellowAccent,
+            state.inProgressTasks.size
+        ),
+        OverViewCard(TaskState.DONE, Theme.color.status.greenAccent, state.doneTasks.size)
+    )
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 12.dp, end = 12.dp, bottom = 12.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        overViews.forEach {
+
+
+            Box(
+                modifier = Modifier
+                    .weight(1f)
+                    .zIndex(999f)
+                    .height(112.dp)
+                    .width(96.dp)
+                    .background(it.background, shape = RoundedCornerShape(20.dp))
+            ) {
+                Column(
+                    modifier
+                        .padding(12.dp)
+                        .background(Color.Transparent, shape = RoundedCornerShape(20.dp))
+                ) {
+                    Box(
+                        Modifier
+                            .size(40.dp)
+                            .background(
+                                color = Color(0x3DFFFFFF),
+                                shape = RoundedCornerShape(12.dp)
+                            )
+                            .insideBorder(1.dp, Color(0x1FFFFFFF), 12.dp),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Icon(
+                            painter = painterResource(
+                                when (it.taskState) {
+                                    TaskState.TODO -> R.drawable.ic_overview_card_todo
+                                    TaskState.IN_PROGRESS -> R.drawable.ic_overview_card_in_progress
+                                    TaskState.DONE -> R.drawable.ic_overview_card_done
+                                }
+                            ), contentDescription = when (it.taskState) {
+                                TaskState.TODO -> stringResource(R.string.to_do)
+                                TaskState.IN_PROGRESS -> stringResource(R.string.in_progress)
+                                TaskState.DONE -> stringResource(R.string.done)
+                            },
+                            tint = Theme.color.textColor.onPrimary,
+                            modifier = Modifier.size(24.dp)
+                        )
+                    }
+                    Text(
+                        text = it.count.toString(),
+                        style = Theme.typography.headline.medium,
+                        color = Theme.color.textColor.onPrimary,
+                    )
+                    Text(
+                        text = when (it.taskState) {
+                            TaskState.TODO -> stringResource(R.string.to_do)
+                            TaskState.IN_PROGRESS -> stringResource(R.string.in_progress)
+                            TaskState.DONE -> stringResource(R.string.done)
+                        },
+                        style = Theme.typography.label.small,
+                        color = Theme.color.textColor.onPrimaryCaption,
+                    )
+
+
+                }
+
+                Icon(
+                    painter = painterResource(R.drawable.overview_card_background),
+                    contentDescription = stringResource(R.string.Overview_Card_Background),
+                    modifier = Modifier
+                        .clip(RoundedCornerShape(topEnd = 20.dp))
+                        .align(Alignment.TopEnd),
+                    tint = Color.Unspecified
+                )
+
+            }
+        }
+    }
+}
+
+private data class OverViewCard(
+    val taskState: TaskState,
+    val background: Color,
+    val count: Int,
+)
---
+++ app/src/main/java/com/baghdad/tudee/ui/composable/PairOfTask.kt
@@ -0,0 +1,45 @@
+package com.baghdad.tudee.ui.composable
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.baghdad.tudee.domain.entity.Task
+import com.baghdad.tudee.ui.screens.homeScreen.HomeScreenUIState
+import com.baghdad.tudee.ui.utils.getCategoryIconPainter
+import com.baghdad.tudee.viewModel.homescreenViewModel.HomeScreenViewModel
+
+@Composable
+fun PairOfTask(
+    modifier: Modifier = Modifier,
+    pair: List<Task>, state: HomeScreenUIState, viewModel: HomeScreenViewModel,
+) {
+    Column(modifier = Modifier.fillMaxWidth()) {
+
+        CategoryTaskCard(
+            title = pair[0].title,
+            description = pair[0].description,
+            priorityTask = pair[0].priority,
+            icon = getCategoryIconPainter(state.categories.firstOrNull { it.id == pair[0].categoryId }!!.image),
+            modifier = modifier
+
+                .padding(bottom = 8.dp)
+        ) {
+            viewModel.getTaskDetailsById(id = pair[0].id)
+        }
+
+        if (pair.size > 1) {
+            CategoryTaskCard(
+                title = pair[1].title,
+                description = pair[1].description,
+                priorityTask = pair[1].priority,
+                icon = getCategoryIconPainter(state.categories.firstOrNull { it.id == pair[1].categoryId }!!.image),
+                modifier = modifier
+            ) {
+                viewModel.getTaskDetailsById(id = pair[0].id)
+            }
+        }
+    }
+}
