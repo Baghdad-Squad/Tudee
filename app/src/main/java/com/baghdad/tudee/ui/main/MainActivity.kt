5c10508e9cc95d76bf6dd2175e35bd5fffe75fac
edrees <muhammed.edrees101@gmail.com>
2025-06-27 03:18:14 +0300

    update app icons and clean up MainActivity imports

--- app/src/main/java/com/baghdad/tudee/ui/main/MainActivity.kt
+++ app/src/main/java/com/baghdad/tudee/ui/main/MainActivity.kt
@@ -4,30 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.animation.AnimatedContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.navigationBarsPadding
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.navigation.NavController
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
-import com.baghdad.tudee.designSystem.theme.Theme
-import com.baghdad.tudee.designSystem.theme.TudeeTheme
-import com.baghdad.tudee.ui.composable.BottomNavigation
-import com.baghdad.tudee.ui.navigation.LocalNavController
-import com.baghdad.tudee.ui.navigation.Route
-import com.baghdad.tudee.ui.navigation.TudeeNavHost
-import com.baghdad.tudee.ui.screens.SplashScreen.SplashScreen
-import org.koin.androidx.compose.koinViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,6 +11,6 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MainScreen()
+        }
     }
-}
 }
