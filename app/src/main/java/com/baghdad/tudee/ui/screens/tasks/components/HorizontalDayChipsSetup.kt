a3e491ee3d4de66a0c1df51121b42d19063f4c8c
Zinah Ashraf <149817756+Arzo-zi@users.noreply.github.com>
2025-07-04 19:29:58 +0300

    Fix/#307 localize month year tasks screen (#322)
    * fix: localize month and year display language in taskHeader
    
    * fix: position of the month and year

--- app/src/main/java/com/baghdad/tudee/ui/screens/tasks/components/HorizontalDayChipsSetup.kt
+++ app/src/main/java/com/baghdad/tudee/ui/screens/tasks/components/HorizontalDayChipsSetup.kt
@@ -50,11 +50,17 @@ fun HorizontalDayChipsSetup(
 
     val dates: List<LocalDate> = uiState.monthDates
 
-
+    val (_,monthName, yearStr) = getLocalizedDateParts(
+        LocalDate(uiState.currentYear, uiState.currentMonth, 1)
+    )
 
     TasksHeader(
-        month = uiState.currentMonth.toString().take(3),
-        year = uiState.currentYear.toString(),
+        month = if ( Locale.getDefault().displayLanguage== "English") {
+            monthName.take(3)
+        } else {
+            monthName
+        },
+        year = yearStr,
         onNextArrowClicked = { tasksInteractionListener.onNextMonthArrowClick() },
         onPreviousArrowClicked = { tasksInteractionListener.onPreviousMonthArrowClick() },
         onMonthClicked = { showDatePicker = true },
